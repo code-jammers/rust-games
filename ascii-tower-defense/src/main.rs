4dcbc4048aa6a04b24da7b6278a90d5e34bd0fa4
Korey Hinton <2801399+koreyhinton@users.noreply.github.com>
2023-10-15 15:18:53 -0400

    draw ascii guy

--- ascii-tower-defense/src/main.rs
+++ ascii-tower-defense/src/main.rs
@@ -4,10 +4,48 @@ use ruscii::keyboard::{Key, KeyEvent};
 use ruscii::spatial::Vec2;
 use ruscii::terminal::Window;
 
+struct Ascii {
+    char: char,
+    xy: Vec2
+}
+struct AsciiArt {
+    ascii_vec: Vec<Ascii>
+}
+
+impl AsciiArt {
+    fn draw(&self, pencil: &mut Pencil) {
+        for a in &self.ascii_vec {
+            pencil.draw_char(a.char, a.xy);
+        }
+    }
+}
+
 fn main() {
     let mut key_events: Vec<KeyEvent> = Vec::new();
     let mut app = App::default();
 
+    let monster_guy = "
+      __
+  ___(**)__
+ (**)    |_)
+  | |  | |`
+  \\ /  \\ /
+";
+
+    let mut x = 0;
+    let mut y = 0;
+    let mut ascii_vec = vec![];
+    for (_, c) in monster_guy.chars().enumerate() {
+        if c == '\n' {
+            x = 0;
+            y += 1;
+            continue;
+        }
+        ascii_vec.push(Ascii { char: c, xy: Vec2::xy(x,y) });
+        x += 1;
+    }
+    let monster = AsciiArt { ascii_vec };
+    
     app.run(|app_state: &mut State, window: &mut Window| {
         for key_event in app_state.keyboard().last_key_events() {
             key_events.push(*key_event);
@@ -16,6 +54,7 @@ fn main() {
             }
         }
         let mut pencil = Pencil::new(window.canvas_mut());
-        pencil.draw_text("Hello, world", Vec2::xy(0, 0));
+        monster.draw(&mut pencil);
+        //pencil.draw_text("Hello, world", Vec2::xy(0, 0));
     });
 }
