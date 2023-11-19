bfe86eeae0068727bbcc4f51c4c0bf022619df9d
Korey Hinton <2801399+koreyhinton@users.noreply.github.com>
2023-11-19 14:07:49 -0500

    tilemap attempts

---
+++ ruler-of-the-reef/src/Reef.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Reef" tilewidth="1075" tileheight="529" tilecount="2" columns="0">
+ <editorsettings>
+  <export target="Reef." format="json"/>
+ </editorsettings>
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="340" height="270" source="../../../../../../Downloads/reef/Fish, crab, jellyfish, shark/PNG/Crab_1/Crab_attack_1_000.png"/>
+ </tile>
+ <tile id="1">
+  <image width="1075" height="529" source="../../../../../../Downloads/reef/Fish, crab, jellyfish, shark/PNG/Shark_1/Shark_attack_1_000.png"/>
+ </tile>
+</tileset>
--- ruler-of-the-reef/src/main.rs
+++ ruler-of-the-reef/src/main.rs
@@ -1,16 +1,48 @@
 use macroquad::prelude::*;
+use macroquad::ui::{hash, root_ui, widgets, Skin};
+use macroquad_tiled as tiled;
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
+    let texture = load_texture("./src/1_game_background.png").await.unwrap();
+    //let sprite_sheet = load_texture("./src/spritesheet.png").await.unwrap();
+    let tiled_map_json = load_string("./src/omg2.json").await.unwrap();
+    //let tileset = load_texture("./src/omg2.png").await.unwrap();
+    //tileset.set_filter(FilterMode::Nearest);
+
+    let crabTexture = load_texture("./src/Crab_attack_1_000.png").await.unwrap();
+    let sharkTexture = load_texture("./src/Shark_attack_1_000.png").await.unwrap();
+    // let tiled_map = tiled::load_map(&tiled_map_json, &[("omg2.png", tileset)], &[]).unwrap();
+    //let mut updImage = true;
+
+    let tiled_map = tiled::load_map(
+        &tiled_map_json,
+        &[("Crab_attack_1_000.png", crabTexture), ("Shark_attack_1_000.png", sharkTexture)],
+        &[],
+    ).unwrap();
+
+    let mut x = 0.0;
+    let mut y = 0.0;
+    let mut dir = 1.;
+
     loop {
+        let w = screen_width();
+        let h = screen_height();
+
+        //draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        //draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        //draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+
+        //draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
         clear_background(RED);
-
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
-
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
-
+        draw_texture(&texture, 0., 0., RED);
+        // tiled_map.draw_tiles("Tile Layer 1", Rect::new(0.0, 0.0, 320.0, 152.0), None);
+        tiled_map.draw_tiles("Tile Layer 1", Rect::new(x, y, 320.0, 152.0), None);
+        x += dir;
+        if x >= w || x <= 0. {
+            dir *= -1.;
+            x += dir*3.;
+        }
         next_frame().await
     }
 }
