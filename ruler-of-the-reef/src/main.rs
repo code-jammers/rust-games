5b0a85f4eb547c36a19e4de98a193207d284eca5
Kasey Hinton <kaseyjameshinton@gmail.com>
2023-12-17 11:28:02 -0700

    Add WASD support; Add up Y movement for shark.

--- ruler-of-the-reef/src/main.rs
+++ ruler-of-the-reef/src/main.rs
@@ -1,6 +1,4 @@
 use macroquad::prelude::*;
-use macroquad::ui::{hash, root_ui, widgets, Skin};
-use macroquad_tiled as tiled;
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
@@ -39,33 +37,20 @@ async fn main() {
 
     let texture = load_texture("./src/1_game_background.png").await.unwrap();
     //let sprite_sheet = load_texture("./src/spritesheet.png").await.unwrap();
-    let tiled_map_json = load_string("./src/omg2.json").await.unwrap();
+    // let tiled_map_json = load_string("./src/omg2.json").await.unwrap();
     //let tileset = load_texture("./src/omg2.png").await.unwrap();
     //tileset.set_filter(FilterMode::Nearest);
 
-    let crabTexture = load_texture("./src/Crab_attack_1_000.png").await.unwrap();
-    let sharkTexture = load_texture("./src/Shark_attack_1_000.png").await.unwrap();
+    // let crabTexture = load_texture("./src/Crab_attack_1_000.png").await.unwrap();
+    // let sharkTexture = load_texture("./src/Shark_attack_1_000.png").await.unwrap();
     // let tiled_map = tiled::load_map(&tiled_map_json, &[("omg2.png", tileset)], &[]).unwrap();
     //let mut updImage = true;
-
-    let tiled_map = tiled::load_map(
-        &tiled_map_json,
-        &[
-            ("Crab_attack_1_000.png", crabTexture),
-            ("Shark_attack_1_000.png", sharkTexture),
-        ],
-        &[],
-    )
-    .unwrap();
-
-    let mut x = 0.0;
-    let mut y = 0.0;
-    let mut dir = 1.;
 
     let mut frame_count = 0;
     let mut animation_frame_count = 0;
     let mut shark_x = 0.0;
     let mut shark_flip_x = false;
+    let mut shark_y = 0.0;
 
     loop {
         let w = screen_width();
@@ -75,27 +60,35 @@ async fn main() {
         draw_texture(&texture, 0., 0., WHITE);
 
         let current_shark_texture = &shark_textures[animation_frame_count];
-       
-        if is_key_down(KeyCode::Right) {
+
+        if is_key_down(KeyCode::Right) || is_key_down(KeyCode::D) {
             shark_x += 5.;
             shark_flip_x = true;
         }
 
-        if is_key_down(KeyCode::Left) {
+        if is_key_down(KeyCode::Left) || is_key_down(KeyCode::A) {
             shark_x -= 5.;
             shark_flip_x = false;
         }
-        
+
+        if is_key_down(KeyCode::Down) || is_key_down(KeyCode::S) {
+            shark_y += 2.;
+        }
+
+        if is_key_down(KeyCode::Up) || is_key_down(KeyCode::W) {
+            shark_y -= 2.;
+        }
+
         let shark_texture_params = DrawTextureParams {
             flip_x: shark_flip_x,
-            dest_size: Some(vec2(1075./3.,529./3.)),
+            dest_size: Some(vec2(1075. / 3., 529. / 3.)),
             ..Default::default()
         };
 
         draw_texture_ex(
             current_shark_texture,
             shark_x,
-            0.,
+            shark_y,
             WHITE,
             shark_texture_params,
         );
